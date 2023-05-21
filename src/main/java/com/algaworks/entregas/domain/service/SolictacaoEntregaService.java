7a423e16d79c65bbaabc92aecb1ee8488f3f95bb
ebpacheco <ebertoncinip@live.com>
2023-05-21 19:36:31 -0300

    Alterando LocalDateTime para OffSetDateTime

--- src/main/java/com/algaworks/entregas/domain/service/SolictacaoEntregaService.java
+++ src/main/java/com/algaworks/entregas/domain/service/SolictacaoEntregaService.java
@@ -1,6 +1,6 @@
 package com.algaworks.entregas.domain.service;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,7 +26,7 @@ public class SolictacaoEntregaService {
 
 		entrega.setCliente(cliente);
 		entrega.setStatus(StatusEntrega.PENDENTE);
-		entrega.setDataPedido(LocalDateTime.now());
+		entrega.setDataPedido(OffsetDateTime.now());
 		return entregaRepository.save(entrega);
 	}
 }
