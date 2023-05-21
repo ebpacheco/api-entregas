fb5905331950a29087fecf869424515838fcca18
ebpacheco <ebertoncinip@live.com>
2023-05-20 21:04:16 -0300

    EqualsAndHashCode em Cliente e EntityManager em ClienteController

--- src/main/java/com/algaworks/entregas/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/entregas/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.entregas.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,21 +13,11 @@ import com.algaworks.entregas.domain.model.Cliente;
 @RestController
 public class ClienteController {
 
+	@PersistenceContext
+	private EntityManager em;
+
 	@GetMapping(path = "/clientes")
 	public List<Cliente> listar() {
-
-		Cliente cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Eduardo");
-		cliente1.setEmail("eduardo@teste.com");
-		cliente1.setTelefone("11 10101-0101");
-
-		Cliente cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Natany");
-		cliente2.setEmail("natany@teste.com");
-		cliente2.setTelefone("11 20202-0202");
-
-		return Arrays.asList(cliente1, cliente2);
+		return em.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 }
