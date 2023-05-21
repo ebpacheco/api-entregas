0c561b08fe8972e664c8c6b17a9544f003ee5d0d
ebpacheco <ebertoncinip@live.com>
2023-05-20 22:16:22 -0300

    Add interface repository e injecao de dependencia autowired

--- src/main/java/com/algaworks/entregas/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/entregas/api/controller/ClienteController.java
@@ -2,22 +2,26 @@ package com.algaworks.entregas.api.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.entregas.domain.model.Cliente;
+import com.algaworks.entregas.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
 
-	@PersistenceContext
-	private EntityManager em;
+//	@PersistenceContext
+//	private EntityManager em;
+
+	@Autowired
+	private ClienteRepository clienteRepository;
 
 	@GetMapping(path = "/clientes")
 	public List<Cliente> listar() {
-		return em.createQuery("from Cliente", Cliente.class).getResultList();
+//		return em.createQuery("from Cliente", Cliente.class).getResultList();
+		return clienteRepository.findAll();
 	}
+
 }
